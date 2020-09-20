ddf4ffaeb2c7e951cb99a4c5f4c8ea42005b8a45
CoreyGaunt <65140255+CoreyGaunt@users.noreply.github.com>
2020-09-19 19:08:52 -0500

    additional update to ETL repo - cleaning up some folder structure

---
+++ Queries/delete_tables_query.sql
@@ -0,0 +1,17 @@
+-- use the below query if you need to drop all tables
+
+drop table actors
+
+drop table titles
+
+drop table act_ttl
+
+drop table directors
+
+drop table dir_ttl
+
+drop table awards
+
+drop table act_award
+
+drop table dir_award
